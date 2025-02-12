475b20f4661561e86ca61fee51c915e1db5573d8
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-02-12 19:26:21 +0100

    c5

--- app/(protected)/dashboard/components/AddPrompt.jsx
+++ app/(protected)/dashboard/components/AddPrompt.jsx
@@ -8,6 +8,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 
 export default function AddPrompt() {
   //ADD THE NEW CLASS TO THE DB AND SET THE TEACHER AS THE USER ACCOUNT
@@ -20,12 +24,26 @@ export default function AddPrompt() {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Create a class 📚</DialogTitle>
+          <DialogTitle>Create a class 📚 </DialogTitle>
           <DialogDescription>
             Fill the fields to create your class
           </DialogDescription>
         </DialogHeader>
-        
+        <form action="" className="w-full flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+                 <Label>Class name :</Label>
+                 <Input name="cname" placeholder="exp : Math class"/>   
+            </div>
+            <div className="flex flex-col gap-2">
+                 <Label>Class niveau:</Label>
+                 <Input name="cniv" placeholder="exp : 2BI"/>   
+            </div>
+            <div className="flex flex-col gap-2">
+                 <Label>Class Code:</Label>
+                 <Input name="ccode" placeholder="*********"/>   
+            </div>
+            <Button>Add</Button>
+        </form>
       </DialogContent>
     </Dialog>
   );
--- app/(protected)/dashboard/components/CreatedContainer.jsx
+++ app/(protected)/dashboard/components/CreatedContainer.jsx
@@ -4,7 +4,7 @@ import AddPrompt from "./AddPrompt";
 export default function CreatedContainer() {
   //GET THE CREATED CLASS FROM THE DB
   return (
-    <div className="flex gap-2 flex-wrap flex-col sm:items-start items-center bg-gradient-to-tr from-black/5 to-blue-100 dark:to-white/10 rounded-lg w-full min-h-screen p-5">
+    <div className="flex gap-2 flex-wrap flex-col sm:items-start items-center bg-gradient-to-tr from-black/5 to-blue-100 dark:to-white/10 rounded-lg w-full min-h-screen sm:p-5 py-5">
       <p className="underline text-xl">Create a class : </p>
       <AddPrompt/>
     </div>
