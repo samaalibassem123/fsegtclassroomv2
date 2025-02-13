29c881df6004a978073091ecf94f5ebc03c94a91
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-02-13 18:23:27 +0100

    Update CHeader.jsx

--- app/(protected)/[class]/components/CHeader.jsx
+++ app/(protected)/[class]/components/CHeader.jsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import React from "react";
 import DMenu from "./CMenu";
+import { BookText, MessageCircle, Video } from "lucide-react";
 
 export default function CHeader() {
   return (
@@ -11,13 +12,24 @@ export default function CHeader() {
         <span className="text-blue-500">Fsegt</span>
         <span className="text-black/70 dark:text-white">Classroom</span>
       </h1>
-
+      <div className="sm:flex hidden gap-6">
+        <Button className="font-extrabold ">
+          Chat <MessageCircle size={32} />
+        </Button>
+        <Button className="font-extrabold ">
+          Meet
+          <Video size={32} />
+        </Button>
+        <Button className="font-extrabold ">
+          Docs <BookText size={32} />
+        </Button>
+      </div>
       <div className="flex items-center justify-center gap-3">
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-        {/*MENU FOR BIG DEVICES*/}
+
         {/*THE LOGOUT BUTTON */}
         <Button className="sm:inline-block hidden font-semibold">Logout</Button>
         <div className=" sm:inline-block hidden">
