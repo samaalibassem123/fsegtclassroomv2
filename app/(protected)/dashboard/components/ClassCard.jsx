39174d68982ecd3e07fe86f2f3bf686efb4beda0
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-02-13 17:33:07 +0100

    c10

--- app/(protected)/dashboard/components/ClassCard.jsx
+++ app/(protected)/dashboard/components/ClassCard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -9,45 +10,83 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { BookText, MessageCircle, Video } from "lucide-react";
+import Link from "next/link";
+import { ClipLoader } from "react-spinners";
 
 export default function ClassCard({ cardType }) {
+  const [EnterClicked, EnterIsClicked] = useState(false);
+  const [docClicked, docIsClicked] = useState(false);
+  const [meetClicked, meetIsClicked] = useState(false);
+  const [chatClicked, chatIsClicked] = useState(false);
+  const [showcode, setShowcode] = useState(false);
+
   return (
-    <Card className="w-[300px] h-[300px]">
-      <CardHeader className="flex flex-row justify-between items-center">
-        <CardTitle className="underline text-xl">Math</CardTitle>
-        <CardDescription className=" bg-gradient-to-tr from-blue-300 to-orange-400 w-fit p-2 rounded-lg float-right">
+    <Card className="w-[300px] h-fit ">
+      <CardHeader className="flex flex-row justify-between items-center py-2  drop-shadow-lg ">
+        <CardTitle className=" text-xl">Math</CardTitle>
+        <CardDescription className=" bg-gradient-to-tr from-blue-300 to-orange-400 w-fit p-2 rounded-lg float-right text-white">
           2BI
         </CardDescription>
       </CardHeader>
       <hr className="mb-5" />
-      <CardContent className="flex flex-col justify-between">
+      <CardContent className="flex flex-col justify-between  drop-shadow-lg">
         <div className="flex gap-3 items-end">
-          <p>Teacher name:</p>
+          <p className="underline">Teacher name:</p>
           <span className="text-sm text-gray-400">Abd salem</span>
         </div>
         <div className="flex gap-3 items-end">
-          <p>ClassCode:</p>
+          <p className="underline">ClassCode:</p>
           <span className="text-sm text-gray-400">xxxxxxx</span>
         </div>
       </CardContent>
-      <CardFooter className="flex flex-col gap-4 items-center justify-center">
+
+      <CardFooter className="flex flex-col gap-4 items-center justify-center  drop-shadow-lg">
         <div className="flex gap-3">
-          <Button>Enter</Button>
+          <Link href={"/calssid"} onClick={() => EnterIsClicked(true)}>
+            <Button className="bg-blue-500">
+              {EnterClicked ? (
+                <ClipLoader color="#a6b9f4" loading={true} size={20} />
+              ) : (
+                "Enter"
+              )}
+            </Button>
+          </Link>
           <Button className="bg-red-400">
             {cardType === "J" ? <span>Quit</span> : <span>Delete</span>}
           </Button>
         </div>
 
+        {/*DIRECT LINKS */}
         <div className="flex gap-3">
-          <Button className="bg-orange-300">
-            <MessageCircle size={32} asChild />
-          </Button>
-          <Button className="bg-black/70">
-            <Video size={32} />
-          </Button>
-          <Button className="bg-blue-300">
-            <BookText size={32} />
-          </Button>
+          <Link href={"/chat"} onClick={() => chatIsClicked(true)}>
+            <Button className="bg-orange-300">
+              {chatClicked ? (
+                <ClipLoader color="#a6b9f4" loading={true} size={20} />
+              ) : (
+                <MessageCircle size={32} />
+              )}
+            </Button>
+          </Link>
+
+          <Link href={"meet"} onClick={() => meetIsClicked(true)}>
+            <Button className="bg-black/70 dark:bg-white/50">
+              {meetClicked ? (
+                <ClipLoader color="#a6b9f4" loading={true} size={20} />
+              ) : (
+                <Video size={32} />
+              )}
+            </Button>
+          </Link>
+
+          <Link href={"docs"} onClick={() => docIsClicked(true)}>
+            <Button className="bg-blue-300">
+              {docClicked ? (
+                <ClipLoader color="#a6b9f4" loading={true} size={20} />
+              ) : (
+                <BookText size={32} />
+              )}
+            </Button>
+          </Link>
         </div>
       </CardFooter>
     </Card>
--- app/(protected)/dashboard/components/containers/CreatedContainer.jsx
+++ app/(protected)/dashboard/components/containers/CreatedContainer.jsx
@@ -5,7 +5,7 @@ import ClassCard from "../ClassCard";
 export default function CreatedContainer() {
   //GET THE CREATED CLASS FROM THE DB
   return (
-    <div className="flex gap-6 flex-wrap   sm:justify-normal justify-center bg-gradient-to-tr from-black/5 to-blue-100 dark:to-white/10 rounded-lg w-full h-fit sm:p-5 py-5">
+    <div className="flex gap-6 flex-wrap  drop-shadow-lg sm:justify-normal justify-center bg-gradient-to-tr from-black/5 to-blue-100 dark:to-white/10 rounded-lg w-full h-fit sm:p-5 py-5">
       <AddPrompt />
       <ClassCard cardType={"C"} />
       <ClassCard cardType={"C"} />
