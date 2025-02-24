118622bd846c1b99757eca84d729e1687089ab49
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-02-24 17:49:38 +0100

    c27

--- app/(protected)/[class]/components/Dashboard.jsx
+++ app/(protected)/[class]/components/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AddCourse from "./AddCourse";
 import CourseCard from "./CourseCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Dashboard() {
   const docs = [
@@ -26,19 +27,33 @@ export default function Dashboard() {
     },
   ];
   return (
-    <div className="w-full min-h-svh p-2 ">
-      {/*ROLE == TEACHER */}
-      <AddCourse />
-      {/*GET COURSES*/}
-      <p className="text-gray-600 text-center text-xl">
-        Create Your First Course Using The Upper Button <br /> 🙄☝
-      </p>
-      <div className=" space-y-3">
-              <CourseCard Docs={docs} />
-      <CourseCard Docs={docs} />
-      <CourseCard Docs={docs} />
+    <Tabs defaultValue="account" className="w-full">
+      <div className="w-full flex items-center justify-center py-4">
+        <TabsList className="sm:w-[600px] w-full ">
+          <TabsTrigger value="course" className="w-full ">
+            Course
+          </TabsTrigger>
+          <TabsTrigger value="Td" className="w-full ">
+            TD
+          </TabsTrigger>
+        </TabsList>
       </div>
-
-    </div>
+      {/*COURSE */}
+      <TabsContent value="course" className="px-5">
+        {/*ROLE == TEACHER */}
+        <AddCourse />
+        {/*GET COURSES*/}
+        <p className="text-gray-600 text-center text-xl">
+          Create Your First Course Using The Upper Button <br /> 🙄☝
+        </p>
+        <div className=" space-y-3">
+          <CourseCard Docs={docs} />
+          <CourseCard Docs={docs} />
+          <CourseCard Docs={docs} />
+        </div>
+      </TabsContent>
+      {/* TD */}
+      <TabsContent value="Td">Change your password here.</TabsContent>
+    </Tabs>
   );
 }
